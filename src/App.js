60b4c18e6d3e6ca19a2cf6136b6e1f38ca2dbf85
zhihao <DZH960126@qq.com>
2022-09-13 15:03:43 +0800

    uptate

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import "antd/dist/antd.less";
-import Login from "./page/login/login";
-import { Routes, Route } from "react-router-dom";
-function App() {
-  return (
-    <div className="app">
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Login />} />
-      </Routes>
-    </div>
-  );
-}
+import React, { Component } from 'react'
 
-export default App;
+export default class App extends Component {
+  render() {
+    return (
+      <div>App</div>
+    )
+  }
+}
