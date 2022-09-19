056ef76b274280dee8fcc776dbe178d479c2f4eb
zhihao <DZH960126@qq.com>
2022-09-19 09:12:33 +0800

    redux

--- src/containers/counter_container.js
+++ src/containers/counter_container.js
@@ -1,7 +1,51 @@
 import Counter from "../components/counter";
 import { connect } from "react-redux";
 import { createIn, createDe, createInA } from "../redux/actions/count_action";
-
+import React, { Component } from "react";
+class Counter extends Component {
+  componentDidMount() {
+    console.log(this.props.increment);
+  }
+  add = () => {
+    let { value } = this.refs.op1;
+    this.props.increment(value * 1);
+  };
+  dec = () => {
+    let { value } = this.refs.op1;
+    this.props.decrement(value * 1);
+  };
+  odd = () => {
+    let { value } = this.refs.op1;
+    if (this.props.count % 2 === 1) {
+      this.props.increment(value * 1);
+    }
+  };
+  async = () => {
+    let { value } = this.refs.op1;
+    this.props.incrementAsync(value * 1, 1000);
+    // setTimeout(() => {
+    //   this.props.increment(value * 1);
+    // }, 1000);
+  };
+  render() {
+    return (
+      <div>
+        <h3>
+          当前计数为{this.props.count},当前有{this.props.person.length}个人
+        </h3>
+        <select name="" id="" ref="op1">
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select>
+        <button onClick={this.add}>+</button>
+        <button onClick={this.dec}>-</button>
+        <button onClick={this.odd}>incrementOdd</button>
+        <button onClick={this.async}>incrementAsync</button>
+      </div>
+    );
+  }
+}
 export default connect(
   (state) => ({ count: state.count, person: state.person }),
   {
